bfbf85d2ed754e27c3ce69394d01af78d6f6bce5
Luis Luna <luis.lunapa@outlook.com>
2021-11-15 13:27:25 -0800

    Setting supported platforms

--- NinjaKit/Package.swift
+++ NinjaKit/Package.swift
@@ -1,12 +1,13 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "NinjaKit",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "NinjaKit",
             targets: ["NinjaKit"]),
@@ -16,8 +17,6 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "NinjaKit",
             dependencies: []),
@@ -26,3 +25,4 @@ let package = Package(
             dependencies: ["NinjaKit"]),
     ]
 )
+
