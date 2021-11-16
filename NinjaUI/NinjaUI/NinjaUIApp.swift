2171d5eaf30bd299b44721ab70401ecc56e4d726
Luis Luna <luis.lunapa@outlook.com>
2021-11-16 10:57:31 -0800

    Created UI client for ios

---
+++ NinjaUI/NinjaUI/NinjaUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NinjaUIApp.swift
+//  NinjaUI
+//
+//  Created by Luis Luna on 11/16/21.
+//
+
+import SwiftUI
+
+@main
+struct NinjaUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
