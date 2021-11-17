b1951471b973e76a47054d371fbc891789d0ce6f
Luis Luna <luis.lunapa@outlook.com>
2021-11-16 20:16:05 -0800

    Added custom progress viwq

--- NinjaUI/NinjaUI/ContentView.swift
+++ NinjaUI/NinjaUI/ContentView.swift
@@ -20,6 +20,7 @@ struct ContentView: View {
                 
                 if viewModel.loading {
                     ProgressView()
+                        .progressViewStyle(DefaultProgressViewStyle())
                 }
             }
         }
